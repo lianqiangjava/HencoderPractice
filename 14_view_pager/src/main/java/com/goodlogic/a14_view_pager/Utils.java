f386c0003cdc1f2052774f62692bad4f95097179
qlq <goodlogic@sina.com>
2019-03-04 11:30:44 +0800

    ViewPager

---
+++ 14_view_pager/src/main/java/com/goodlogic/a14_view_pager/Utils.java
@@ -0,0 +1,28 @@
+package com.goodlogic.a14_view_pager;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.TypedValue;
+
+public class Utils {
+    public static float dp2px(float dp){
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,Resources.getSystem().getDisplayMetrics());
+    }
+
+    public static Bitmap getAvatar(int width,Context context){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(context.getResources(),R.drawable.head,options);
+        options.inJustDecodeBounds = false;
+        options.inDensity = options.outWidth;
+        options.inTargetDensity = width;
+        return BitmapFactory.decodeResource(context.getResources(),R.drawable.head,options);
+
+    }
+
+    public static float getZForCamera(){
+        return -8 * Resources.getSystem().getDisplayMetrics().density;
+    }
+}
